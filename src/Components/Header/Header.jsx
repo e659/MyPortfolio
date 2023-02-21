2a0337ee910fb427e8b3420378408cc77acfe989
e659 <emoomohamed419@gmail.com>
2023-02-21 21:40:04 +0200

    fix responsive

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -120,21 +120,7 @@ export default function Header({ backgroundColor = "transparent" }) {
       </Navbar> */}
 
       <nav className="navbar navbar-expand-lg fixed-bottom">
-        {/* <a className="navbar-brand" href="#">
-          Navbar
-        </a> */}
         <div className="container">
-          {/* <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarSupportedContent"
-            aria-controls="navbarSupportedContent"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span className="navbar-toggler-icon"></span>
-          </button> */}
           <div
             className="collapse navbar-collapse d-flex justify-content-center"
             id="navbarSupportedContent"
@@ -190,7 +176,7 @@ export default function Header({ backgroundColor = "transparent" }) {
                 </Link>
               </li>
 
-              <li className="nav-item px-4">
+              <li className="nav-item px-3">
                 <Link
                   style={{ cursor: "pointer" }}
                   activeStyle={activeStyle}
